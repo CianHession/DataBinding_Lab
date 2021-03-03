c5f7de75976feb6941c7dc9c3e4b4c5fd155a3be
Cian <g00379799@gmit.ie>
2021-03-03 11:07:56 +0000

    Lab Upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-bind-lab';
+  numClicks: number = 0;
+  hidden = false;
+
+  count() {
+    this.numClicks++;
+  }
+  show() {
+    if (this.hidden == true) {
+      this.hidden = false;
+    }
+    else {
+      this.hidden = true;
+    }
+  }//show
+  
 }
